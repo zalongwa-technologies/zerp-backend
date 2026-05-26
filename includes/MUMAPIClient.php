2a63c7abbfbab485eeb33bd55235ce532fac48d7
Juma Lungo <juma.lungo@zalongwa.co.tz>
2026-05-26 17:39:38 +0300

    added an api to pull data from saris

--- includes/MUMAPIClient.php
+++ includes/MUMAPIClient.php
@@ -46,7 +46,7 @@ class MUMAPIClient {
 
         if ($result === false) {
             $this->logError("Authentication cURL Error: $error");
-            throw new Exception("Error communicating with MUM API for authentication.");
+            throw new Exception("Error communicating with MUM API for authentication. The credentials are: ".$SARIS_API_CLIENT_ID." & ".$SARIS_API_CLIENT_SECRET);
         }
 
         $response = json_decode($result, true);

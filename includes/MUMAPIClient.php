460ee39007160cc2237bd4d34bc702e8d25a42bb
Juma Lungo <juma.lungo@zalongwa.co.tz>
2026-05-27 15:49:42 +0300

    updated

--- includes/MUMAPIClient.php
+++ includes/MUMAPIClient.php
@@ -83,7 +83,7 @@ class MUMAPIClient {
 
         if ($result === false) {
             $this->logError("Authentication cURL Error: $error");
-            throw new Exception("Error communicating with MUM API for authentication.");
+            throw new Exception("Error communicating with MUM API for authentication. URL: ".$url);
         }
 
         $response = json_decode($result, true);

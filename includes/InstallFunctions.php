bac5e9e33c3e498f2a69f01b12b0854724193bb1
Juma Lungo <juma.lungo@zalongwa.co.tz>
2026-05-29 08:03:15 +0300

    updated Install Function

--- includes/InstallFunctions.php
+++ includes/InstallFunctions.php
@@ -497,9 +497,8 @@ function UploadData($Demo, $AdminPassword, $AdminUser, $Email, $Language, $CoA, 
 		if (DB_error_no() == 0) {
 			echo '<div class="success">' . __('The database update revision has been inserted') . '</div>';
 		} else {
-			echo $SQL;
 			$Errors++;
-			echo '<div class="error">' . __('There was an error inserting the DB revision number') . ' - ' . DB_error_msg() . '</div>';
+			echo '<div class="error">' . __('There was an error inserting the DB revision number') . ' - ' . DB_error_msg() . '</div>'.$SQL;
 		}
 		flush();
 
